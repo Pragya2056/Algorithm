0fad8861bd93eec48bca58ed65c24983273f45f9
Pragya Sapkota <pragyasapkota2056@gmail.com>
2022-05-13 11:43:55 +0545

    Create second way.js

---
+++ Day 3: Two Sum II - Input Array Is Sorted/second way.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let start = 0;
+    let end = numbers.length - 1;
+    
+    while(start < end) {
+        const sum = numbers[start] + numbers[end];
+        
+        if(sum === target) return [start + 1, end + 1];  
+        else if (target < sum) end--;  
+        else start++;  
+        
+    }
+};
